b42f77da9377fda4d85773e90f416563adbc0555
Elias Treadway <eliastreadwayofficial@gmail.com>
2025-06-15 15:52:29 -0400

    Name reordering + Autocomplete Results

--- src/pages/LandingPage.tsx
+++ src/pages/LandingPage.tsx
@@ -1,8 +1,9 @@
 import {
-  Alert,
   Autocomplete,
   Box,
+  Card,
   Container,
+  Divider,
   TextField,
   Typography,
 } from "@mui/material";
@@ -14,11 +15,18 @@ import { useUserContext } from "../contexts/user-context";
 
 type User = {
     name: string
+    address: {
+      street?: string
+      suite?: string
+      zipcode?: string
+      city?: string
+    }
 };
 
 const LandingPage = () => {
   const { fetchUsers } = useUserContext();
-  const [users, setUsers] = useState([]);
+  const [users, setUsers] = useState<User[]>([]);
+  const [selectedUser, setSelectedUser] = useState<User | null>(null);
   const [isLoading, setLoading] = useState(false);
 
   useEffect(() => {
@@ -36,10 +44,16 @@ const LandingPage = () => {
 
       setLoading(true);
 
-      const users = await fetchUsers();
+      const users: User[] = await fetchUsers();
+
+      users.push({"name": "Mx. José Antonio de la Cruz Martínez Hernández", "address": { "street": "Norberto Crossing", "suite": "", "zipcode": "2305-1337"}})
+
+      users.push({"name": "Mr. James Von Doe III", "address": { "street": "Norberto Crossing", "suite": "", "zipcode": "2305-1337"}})
+
+      const sortedUsers = sortRenameUser(users);
 
       if (mounted) {
-        setUsers(users);
+        setUsers(sortedUsers);
         setLoading(false);
       }
     } catch (err) {
@@ -48,6 +62,40 @@ const LandingPage = () => {
         setLoading(false);
       }
     }
+  }
+
+  const sortRenameUser = (users: User[]) => {
+
+    // get name
+    // detect title if present
+    // if title is present the next word is the first name and anything after is the middle, last, suffix etc.
+
+    // move the title and first name to the end and add a comma as a prefix.
+  
+    const renamedUsers = [...users].map(
+      (user) => {
+        
+        let title = user.name.match(/(Mr\.|Mrs\.|Ms\.|Mx\.?)(?=\s)/i)
+
+        let firstNameRegex = /\p{L}+(?=\s)/u
+        let firstName = firstNameRegex.exec(user.name);
+
+        if (title) {
+          firstNameRegex = /(?<=\s)\p{L}+(?=\s)/u
+          firstName = firstNameRegex.exec(user.name)
+        }
+
+        if (firstName) {
+          user.name = `${user.name.substring(firstName.index + firstName[0].length + 1)}, ${firstName[0]} ${title ? `(${title[0]})` : ''}`
+        }
+        
+        return user
+      }
+    )
+
+    const sortedUsers = [...renamedUsers].sort((a , b) => a.name.localeCompare(b.name));
+
+    return sortedUsers
   }
 
   return (
@@ -65,7 +113,7 @@ const LandingPage = () => {
         <Box
           sx={{
             width: "90%",
-            marginBottom: "10%",
+            height: "420px",
             display: "flex",
             flexDirection: "column",
             alignItems: "center",
@@ -103,18 +151,50 @@ const LandingPage = () => {
 
           <Autocomplete
             options={users}
-            sx={{ width: "100%" }}
+            sx={{ width: "100%", marginBottom: "20px" }}
             getOptionLabel={(option: User) => option.name}
-            renderInput={(params) => <TextField {...params} label={isLoading ? "Loading..." : "Movie"} />}
+            renderInput={(params) => <TextField {...params} label={isLoading ? "Loading..." : "User"} />}
+            onChange={(event, value) => {
+              setSelectedUser(value);
+            }}
+            slotProps={{
+              listbox: {
+                sx: {
+                  '& .MuiAutocomplete-option': {
+                    borderBottom: '1px solid #ddd',
+                    py: 1,
+                    px: 2,
+                    '&:last-of-type': {
+                      borderBottom: 'none',
+                    },
+                  },
+                },
+              },
+            }}
             renderOption={(props, option: User) => {
-              const { key, ...optionProps } = props;
+              const { key } = props;
               return (
-                <Box key={key}>
-                  {option.name}
-                </Box>
+                <li {...props} key={key}>
+                  <Box sx={{ paddingY: "5px", paddingX: "5px" }}>
+                    <Typography sx={{ fontWeight: "bold"}}>{option.name}</Typography>
+                  </Box>
+                </li>
               )
             }}
           />
+          {selectedUser && (
+            <Card sx={{ width: "100%", paddingX: "10px", boxSizing: "border-box" }}>
+              <Typography variant="h5" sx={{ fontWeight: "bold", marginY: "10px"}}>{selectedUser.name}</Typography>
+              <Divider />
+              <Box sx={{ paddingY: "5px" }}>
+                <Typography>{selectedUser.address.street}</Typography>
+                <Typography>{selectedUser.address.suite}</Typography>
+                <Typography>{selectedUser.address.city}</Typography>
+                <Typography>{selectedUser.address.zipcode}</Typography>
+              </Box>
+            </Card>
+          )}
+
         </Box>
       </Container>
     </>
