0e2798c6433bff847f360b2228fc1929491b2b79
Elias Treadway <45334183+McDiamund@users.noreply.github.com>
2025-06-15 23:35:23 -0400

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,11 @@
 import { defineConfig } from 'vitest/config'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   test: {
     environment: 'jsdom',
-    globals: true, // optional: allows using describe/it/test without import
-    setupFiles: './src/setupTests.ts', // optional: where you set up jest-dom
+    globals: true,
+    setupFiles: './src/setupTests.ts',
   },
 })
